140fc592a67efe99b35ee4781084970539693d67
generaldave <icepicktoeye@gmail.com>
2016-11-21 13:44:09 -0500

    Working on BaseTypes

---
+++ BaseTypes.py
@@ -0,0 +1,292 @@
+########################################################################
+#                                                                      #
+# David Fuller                                                         #
+#                                                                      #
+# BaseTypes for Loot Filter Creator for Path of Exile                  #
+#                                                                      #
+# Created on 2016-11-21                                                #
+#                                                                      #
+########################################################################
+
+########################################################################
+#                                                                      #
+#                                FLASKS                                #
+#                                                                      #
+########################################################################
+
+# Life Flask BaseTypes
+lifeFlaskBaseTypes = []
+lifeFlaskBaseTypes.append("All")                     # 0
+lifeFlaskBaseTypes.append("Small Life Flask")        # 1
+lifeFlaskBaseTypes.append("Medium Life Flask")       # 2
+lifeFlaskBaseTypes.append("Large Life Flask")        # 3
+lifeFlaskBaseTypes.append("Greater Life Flask")      # 4
+lifeFlaskBaseTypes.append("Grand Life Flask")        # 5
+lifeFlaskBaseTypes.append("Giant Life Flask")        # 6
+lifeFlaskBaseTypes.append("Colossal Life Flask")     # 7
+lifeFlaskBaseTypes.append("Sacred Life Flask")       # 8
+lifeFlaskBaseTypes.append("Hallowed Life Flask")     # 9
+lifeFlaskBaseTypes.append("Sanctified Life Flask")   # 10
+lifeFlaskBaseTypes.append("Divine Life Flask")       # 11
+lifeFlaskBaseTypes.append("Eternal Life Flask")      # 12
+
+# Mana Flask BaseTypes
+manaFlaskBaseTypes = []
+manaFlaskBaseTypes.append("All")                     # 0
+manaFlaskBaseTypes.append("Small Mana Flask")        # 1
+manaFlaskBaseTypes.append("Medium Mana Flask")       # 2
+manaFlaskBaseTypes.append("Large Mana Flask")        # 3
+manaFlaskBaseTypes.append("Greater Mana Flask")      # 4
+manaFlaskBaseTypes.append("Grand Mana Flask")        # 5
+manaFlaskBaseTypes.append("Giant Mana Flask")        # 6
+manaFlaskBaseTypes.append("Colossal Mana Flask")     # 7
+manaFlaskBaseTypes.append("Sacred Mana Flask")       # 8
+manaFlaskBaseTypes.append("Hallowed Mana Flask")     # 9
+manaFlaskBaseTypes.append("Sanctified Mana Flask")   # 10
+manaFlaskBaseTypes.append("Divine Mana Flask")       # 11
+manaFlaskBaseTypes.append("Eternal Mana Flask")      # 12
+
+# Hybrid Flask BaseTypes
+hybridFlaskBaseTypes = []
+hybridFlaskBaseTypes.append("All")                     # 0
+hybridFlaskBaseTypes.append("Small Hybrid Flask")      # 1
+hybridFlaskBaseTypes.append("Medium Hybrid Flask")     # 2
+hybridFlaskBaseTypes.append("Large Hybrid Flask")      # 3
+hybridFlaskBaseTypes.append("Colossal Hybrid Flask")   # 4
+hybridFlaskBaseTypes.append("Sacred Hybrid Flask")     # 5
+hybridFlaskBaseTypes.append("Hallowed Hybrid Flask")   # 6
+
+# Utility Flasks BaseTypes
+utilityFlaskBaseTypes = []
+utilityFlaskBaseTypes.append("All")                 # 0
+utilityFlaskBaseTypes.append("Quicksilver Flask")   # 1
+utilityFlaskBaseTypes.append("Bismuth Flask")       # 2
+utilityFlaskBaseTypes.append("Stibnite Flask")      # 3
+utilityFlaskBaseTypes.append("Amethyst Flask")      # 4
+utilityFlaskBaseTypes.append("Ruby Flask")          # 5
+utilityFlaskBaseTypes.append("Sapphire Flask")      # 6
+utilityFlaskBaseTypes.append("Topaz Flask")         # 7
+utilityFlaskBaseTypes.append("Silver Flask")        # 8
+utilityFlaskBaseTypes.append("Aquamarine Flask")    # 9
+utilityFlaskBaseTypes.append("Granite Flask")       # 10
+utilityFlaskBaseTypes.append("Jade Flask")          # 11
+utilityFlaskBaseTypes.append("Quartz Flask")        # 12
+utilityFlaskBaseTypes.append("Sulphur Flask")       # 13
+utilityFlaskBaseTypes.append("Basalt Flask")        # 14
+
+# Critical Utility Flasks BaseTypes
+criticalUtilityFlaskBaseTypes = []
+criticalUtilityFlaskBaseTypes.append("All")             # 0
+criticalUtilityFlaskBaseTypes.append("Diamond Flask")   # 1
+
+########################################################################
+#                                                                      #
+#                               CURRENCY                               #
+#                                                                      #
+########################################################################
+
+currencyBaseTypes = []
+currencyBaseTypes.append("All")                                 # 0
+currencyBaseTypes.append("Albino Rhoa Feather")                 # 1
+currencyBaseTypes.append("Apprentice Cartographer's Sextant")   # 2
+currencyBaseTypes.append("Armourer's Scrap")                    # 3
+currencyBaseTypes.append("Blacksmith's Whetstone")              # 4
+currencyBaseTypes.append("Blessed Orb")                         # 5
+currencyBaseTypes.append("Cartographer's Chisel")               # 6
+currencyBaseTypes.append("Chaos Orb")                           # 7
+currencyBaseTypes.append("Chromatic Orb")                       # 8
+currencyBaseTypes.append("Divine Orb")                          # 9
+currencyBaseTypes.append("Exalted Orb")                         # 10
+currencyBaseTypes.append("Gemcutter's Prism")                   # 11
+currencyBaseTypes.append("Glassblower's Bauble")                # 12
+currencyBaseTypes.append("Jeweller's Orb")                      # 13
+currencyBaseTypes.append("Journeyman Cartographer's Sextant")   # 14
+currencyBaseTypes.append("Master Cartographer's Sextant")       # 15
+currencyBaseTypes.append("Mirror of Kalandra")                  # 16
+currencyBaseTypes.append("Orb of Alchemy")                      # 17
+currencyBaseTypes.append("Orb of Alteration")                   # 18
+currencyBaseTypes.append("Orb of Augmentation")                 # 19
+currencyBaseTypes.append("Orb of Chance")                       # 20
+currencyBaseTypes.append("Orb of Fusing")                       # 21
+currencyBaseTypes.append("Orb of Regret")                       # 22
+currencyBaseTypes.append("Orb of Scouring")                     # 23
+currencyBaseTypes.append("Orb of Transmutation")                # 24
+currencyBaseTypes.append("Portal Scroll")                       # 25
+currencyBaseTypes.append("Regal Orb")                           # 26
+currencyBaseTypes.append("Scroll of Wisdom")                    # 27
+currencyBaseTypes.append("Silver Coin")                         # 28
+currencyBaseTypes.append("Stacked Deck")                        # 29
+currencyBaseTypes.append("Vaal Orb")                            # 30
+
+########################################################################
+#                                                                      #
+#                          DIVINATION CARDS                            #
+#                                                                      #
+########################################################################
+
+divinationBaseTypes = []
+divinationBaseTypes.append("All")   # 0
+divinationBaseTypes.append("Abandoned Wealth")   # 0
+divinationBaseTypes.append("The Aesthete")   # 0
+divinationBaseTypes.append("Anarchy's Price")   # 0
+divinationBaseTypes.append("The Arena Champion")   # 0
+divinationBaseTypes.append("The Artist")   # 0
+divinationBaseTypes.append("Assassin's Favour")   # 0
+divinationBaseTypes.append("Audacity")   # 0
+divinationBaseTypes.append("The Avenger")   # 0
+divinationBaseTypes.append("The Battle Born")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 0
+divinationBaseTypes.append("")   # 164
